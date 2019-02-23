cee4c2bb6eecd54044dbc4bd20e79fcb15fb86c4
Skyler Rankin <skylerrankin@gmail.com>
2019-02-23 14:23:46 -0500

    saved

--- core/src/com/aeiaton/classes/LaserGrid.java
+++ core/src/com/aeiaton/classes/LaserGrid.java
@@ -7,25 +7,19 @@ import com.badlogic.gdx.math.Vector2;
 
 public class LaserGrid {
     
-    private Vector2[] points;
-    private int[] dir;
+    private static Vector2[] points;
+    private static int[] dir;
     private int end;
-    public float percent = 0.0f;
-    public float total_length = 0;
     
     public LaserGrid(Vector2[] p, int[] d, int e) {
         points = p;
         dir = d;
         end = e;
-        for (int i = 0; i < points.length-1; ++i) {
-            total_length += Math.sqrt(Math.pow(p[i].x - p[i+1].x, 2) + Math.pow(p[i].x - p[i+1].x, 2));
-        }
     }
     
     public List<List<Vector2>> getLines() {
-        int sections = (int)((points.length-1)*percent) + 1;
         List<List<Vector2>> lines = new ArrayList<>();
-        for (int i = 0; i < sections-1; ++i) {
+        for (int i = 0; i < points.length-1; ++i) {
             List<Vector2> line = new ArrayList<>();
             line.add(points[i]);
             line.add(points[i+1]);
@@ -34,15 +28,31 @@ public class LaserGrid {
         return lines;
     }
     
-    public boolean willCollide(float x, float y, int dir) {
-        for (Vector2 v : points) {
-            if (Math.abs(v.x - x) < 0.1 && (v.y - y)*dir > 0) {
-                return true;
-            } else if (Math.abs(v.y - y) < 0.1 && (v.x - x)*dir > 0) {
-                return true;
+    public static int willCollide(Vector2 v1, int dir) {
+        Vector2 v;
+        for (int i = 0; i < points.length; i++) {
+            v = points[i];
+            if (Math.abs(v.x - v1.x) < 0.1 && (v.y - v1.y)*dir > 0) {
+                return i;
+            } else if (Math.abs(v.y - v1.y) < 0.1 && (v.x - v1.x)*dir > 0) {
+                return i;
             }
         }
-        return false;
+        return -1;
     }
-
+    
+    public static List<Vector2> compute (Vector2 v1, int dir1) {
+        int i = willCollide(v1, dir1);
+        ArrayList<Vector2> l = new ArrayList<Vector2>();
+        Vector2 v;
+        int d;
+        while (i != -1) {
+            v = points[i];
+            d = dir[i];
+            l.add(v);
+            i = willCollide(v, d);
+        }
+        return l;
+    }
+    
 }
